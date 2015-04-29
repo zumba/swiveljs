b36a74feff1521333ee1a73e5e119c1c12daa780
Stephen Young <young.steveo@gmail.com>
2015-04-29 14:34:37 -0400

    Initial Swivel object

---
+++ src/api.js
@@ -0,0 +1,14 @@
+
+/**
+ * Swivel constructor
+ */
+var Swivel = function Swivel() {
+    if (!(this instanceof Swivel)) {
+        return new Swivel();
+    }
+};
+
+/**
+ * Swivel prototype
+ */
+Swivel.prototype = { };
