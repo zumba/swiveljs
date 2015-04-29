ffeda8b89ace78a1e134c7dc5d16c805807c6eae
Stephen Young <young.steveo@gmail.com>
2015-04-29 14:27:31 -0400

    Initial dist files

---
+++ dist/swivel.js
@@ -0,0 +1,13 @@
+;(function SwivelJS(undefined) {
+    'use strict';
+    /**
+     * SwivelJS v0.0.1 - 2015-04-29
+     * Strategy driven, segmented feature toggles
+     *
+     * Copyright (c) 2015 Zumba&reg;
+     * Licensed MIT
+     */
+    
+    
+    
+}.call(this));
