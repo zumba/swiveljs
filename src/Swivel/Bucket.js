957b634e26234a8288eeb2f0e1d48802b5166800
Stephen Young <young.steveo@gmail.com>
2015-04-30 07:39:58 -0400

    Added ordinal buckets to Bucket

--- src/Swivel/Bucket.js
+++ src/Swivel/Bucket.js
@@ -5,3 +5,18 @@
 var Bucket = function Bucket() {
 
 };
+
+/**
+ * Ordinal Bitmasks
+ */
+Bucket.FIRST = 1;
+Bucket.SECOND = 2;
+Bucket.THIRD = 4;
+Bucket.FOURTH = 8;
+Bucket.FIFTH = 16;
+Bucket.SIXTH = 32;
+Bucket.SEVENTH = 64;
+Bucket.EIGHTH = 128;
+Bucket.NINTH = 256;
+Bucket.TENTH = 512;
+Bucket.ALL = 1023;
