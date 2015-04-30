ee9d02a7f5597d9c93c3a8de59a455d581a8c7bd
Stephen Young <young.steveo@gmail.com>
2015-04-30 14:40:11 -0400

    Removed the ordinal bitmasks They were not really useful, and doing so removed approx 120 bytes.

--- src/Swivel/Bucket.js
+++ src/Swivel/Bucket.js
@@ -8,22 +8,6 @@ var Bucket = function Bucket(featureMap, index) {
 };
 
 /**
- * Ordinal Bitmasks
- */
-Bucket.FIRST = 1;
-Bucket.SECOND = 2;
-Bucket.THIRD = 4;
-Bucket.FOURTH = 8;
-Bucket.FIFTH = 16;
-Bucket.SIXTH = 32;
-Bucket.SEVENTH = 64;
-Bucket.EIGHTH = 128;
-Bucket.NINTH = 256;
-Bucket.TENTH = 512;
-Bucket.ALL = 1023;
-
-
-/**
  * Check if a behavior is enabled for a particular context/bucket combination
  *
  * @param Behavior behavior
